d034a45a645ee58a42cce5ac2372b60225750cb3
boynuibon3s <boynuibon3s@gmail.com>
2020-09-23 10:20:55 +0700

    edit after undo

--- Hotel_/Hotel_/Form1.cs
+++ Hotel_/Hotel_/Form1.cs
@@ -17,6 +17,9 @@ namespace Hotel_
             InitializeComponent();
         }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
+        }
     }
 }
